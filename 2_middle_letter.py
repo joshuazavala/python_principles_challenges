5459a41362007508007ff0e82570c1a022d76d34
Joshua Zavala <54443512+joshuazavala@users.noreply.github.com>
2022-08-13 23:22:17 -0500

    Add files via upload

--- 2_middle_letter.py
+++ 2_middle_letter.py
@@ -1,16 +1,13 @@
 # Function for finding the middle letter in a string and returning it
-def mid (x):
-    not_even = ""
-    even = len (x) % 2
-    mid_number = len (x) // 2
-    
-    if even == 0:
-        return not_even
+def mid (word):
+    if len (word) % 2 == 0:
+        return ""
     else:
-        return x [mid_number]
+        middle_number = len (word) // 2
+        return word [middle_number]
 
 # User input
-user_input = input ("Type: ")
+user_word = input ("Type: ")
 
 # Calling the function with user input
-print (mid (user_input))
+print (mid (user_word))
